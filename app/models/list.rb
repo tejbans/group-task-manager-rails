cb98066f8010cc4c79351de28fdab2a1dd8d7055
tejbans <singh.tejbans@gmail.com>
2016-06-14 12:25:22 -0700

    refactor

--- app/models/list.rb
+++ app/models/list.rb
@@ -6,9 +6,7 @@ class List < ActiveRecord::Base
  def tasks_attributes=(task_attributes)
   task_attributes.values.each do |task_attribute|
     if task_attribute[:title].present? 
-      if !tasks.detect{|t| t.title.strip == task_attribute[:title].strip}
         task = tasks.build(task_attribute)
-      end
     end
   end
 end
