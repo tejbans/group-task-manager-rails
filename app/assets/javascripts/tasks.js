515e540c5d363cb00c5b39e185bd70bf1999dc4d
tejbans <singh.tejbans@gmail.com>
2016-07-13 00:26:34 -0700

    refactor task.js

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,18 +1,19 @@
 
 $(function(){
-  var tasks=[];
 $("a[href='/mylists']").on("click", function(e) {
       e.preventDefault();
+      var tasks=[];
+      var tasklists="<h2> My Task Lists </h2>";
     $.get("/mylists.json", function(data){
         data.forEach(function(task_attr){
           var task = new Task(task_attr)
-//          task.renderTask($('.col-md-8'));
           tasks.push(task);
         });
-        $('.col-md-8').html("<h2> My Task Lists </h2>");
+       
         tasks.forEach(function(task){
-          task.renderTask($('.col-md-8'));
+           tasklists += task.renderTask();
         });
+        $('.col-md-8').html(tasklists);
     });
   });
 });
@@ -29,7 +30,7 @@ function Task(attributes){
     this.user_name= attributes.user_info;
   }
 
-Task.prototype.renderTask = function(selector){
+Task.prototype.renderTask = function(){
    var taskList = "";
             taskList +=  "<h3><a href=/lists/"+ this["list_id"]+">"+ this["list_name"]+"</a></h3>"  ;
              taskList += "<ul><li>" + this["title"] + ' | ' ;
@@ -37,5 +38,5 @@ Task.prototype.renderTask = function(selector){
              taskList +=  this["due_date"] + ' | ' ;
              taskList +=  "Assigned to: " + this["user_name"] + '</li><br>' ;
             taskList += '</ul>';
-      selector.append(taskList);
+      return taskList
     }
